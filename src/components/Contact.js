62b761ad50ff0d0556f650cfd0774d2347c99d71
K.Durga prasanth <99675960+Durgaprasanth9492@users.noreply.github.com>
2023-11-03 12:52:50 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: prasanth.21bce7103@vitapstudent.ac.in</p>
+      <p>Phone: +91 (9154667332)</p>
+    </section>
+  );
+}
+
+export default Contact;
